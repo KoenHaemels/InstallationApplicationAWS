8090439d893425709abba739929d8bf2ab900f05
KoenHaemels <koenhaemels@gmail.com>
2017-05-05 09:54:29 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -13,9 +13,10 @@ clear
 
 #Installation S3fs
 sudo apt-get install automake autotools-dev g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config -y
-git clone https://github.com/s3fs-fuse/s3fs-fuse.gitcd s3fs-fuse
-./autogen.sh
-./configure
+sudo git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+cd s3fs-fuse
+sudo ./autogen.sh
+sudo ./configure
 make
 sudo make install
 cd ..
