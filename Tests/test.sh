e0ec8bf97776c347d6965f6e2d0779f23d282267
KoenHaemels <koenhaemels@gmail.com>
2017-05-05 10:16:45 +0200

    Update test.sh

--- Tests/test.sh
+++ Tests/test.sh
@@ -2,9 +2,9 @@
 #Test script
 #Check yaml files
 #Installation yamllint
-#sudo apt-get update
-#sudo apt-get install yamllint -y
-#clear
+sudo apt-get update
+sudo apt-get install yamllint -y
+clear
 
 /usr/bin/yamllint -c /Tests/configyamllint.yml /var/InstallationApplicationAWS
 
